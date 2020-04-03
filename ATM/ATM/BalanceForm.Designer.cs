fe4ef8611e4c52531260870c1c346846fce6674e
budryte <43143936+budryte@users.noreply.github.com>
2020-04-03 13:14:10 +0100

    balance form design

---
+++ ATM/ATM/BalanceForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ATM
+{
+    partial class BalanceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDisplayBalance = new System.Windows.Forms.Label();
+            this.labelBalance = new System.Windows.Forms.Label();
+            this.buttonGoBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelDisplayBalance
+            // 
+            this.labelDisplayBalance.AutoSize = true;
+            this.labelDisplayBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDisplayBalance.ForeColor = System.Drawing.Color.White;
+            this.labelDisplayBalance.Location = new System.Drawing.Point(211, 136);
+            this.labelDisplayBalance.Name = "labelDisplayBalance";
+            this.labelDisplayBalance.Size = new System.Drawing.Size(388, 39);
+            this.labelDisplayBalance.TabIndex = 0;
+            this.labelDisplayBalance.Text = "Your account balance is:";
+            // 
+            // labelBalance
+            // 
+            this.labelBalance.AutoSize = true;
+            this.labelBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBalance.ForeColor = System.Drawing.Color.White;
+            this.labelBalance.Location = new System.Drawing.Point(297, 198);
+            this.labelBalance.Name = "labelBalance";
+            this.labelBalance.Size = new System.Drawing.Size(199, 55);
+            this.labelBalance.TabIndex = 1;
+            this.labelBalance.Text = "£123.45";
+            // 
+            // buttonGoBack
+            // 
+            this.buttonGoBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGoBack.Location = new System.Drawing.Point(35, 385);
+            this.buttonGoBack.Name = "buttonGoBack";
+            this.buttonGoBack.Size = new System.Drawing.Size(120, 55);
+            this.buttonGoBack.TabIndex = 2;
+            this.buttonGoBack.Text = "BACK";
+            this.buttonGoBack.UseVisualStyleBackColor = true;
+            this.buttonGoBack.Click += new System.EventHandler(this.buttonGoBack_Click);
+            // 
+            // BalanceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 471);
+            this.Controls.Add(this.buttonGoBack);
+            this.Controls.Add(this.labelBalance);
+            this.Controls.Add(this.labelDisplayBalance);
+            this.Name = "BalanceForm";
+            this.Text = "BalanceForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDisplayBalance;
+        private System.Windows.Forms.Label labelBalance;
+        private System.Windows.Forms.Button buttonGoBack;
+    }
+}
